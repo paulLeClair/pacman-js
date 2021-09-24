0652f9ce7b8ed2f033218efc275f0ceaa3a4240d
paul leclair <bitepunch@gmail.com>
2021-09-24 16:40:16 -0600

    time for some R&D
    I need to think about how I'm gonna try and structure everything before I continue, so I'll commit these tiny changes for now

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,14 +1,63 @@
 import React, { useState, useEffect } from 'react'
+import './Game.scss'
+
+// not sure how much of a point there is to doing this, but for now it's kool
+const gameClassName = "Game";
+
+const Game = ({gameShouldRun}) => {
+
+    // maybe i'll just try to have the game exist in a few possible states...
+    const PossibleGameStates = {
+        INTRO: 0,
+        GAMEPLAY: 1,
+        GAMEOVER: 2,
+    };
+    const [gameState, setGameState] = useState(PossibleGameStates.INTRO);
 
 
-const Game = (props) => {
+    // i'll separate each game state into a separate function maybe...? not sure yet how best to organize it
+        // this might get too unwieldy, but i'm hoping I can just maintain most of the state here and 
+        // the subcomponents can do a lot of the heavy lifting...
+    // gotta do some thinking before i continue
+    
+    const renderIntro = () => {
+        
+
+        return (
+            <div className={gameClassName}>
+                
+            </div>
+        );
+    }
+
+    const renderGameplay = () => {
+        return (
+            <div className={gameClassName}> 
+                
+            </div>
+        );
+    }
+
+    const renderGameOver = () => {
+        return (
+            <div className={gameClassName}>
+
+            </div>
+        );
+    }
 
 
+    switch (gameState) {
+        case PossibleGameStates.INTRO: return renderIntro();
+        case PossibleGameStates.GAMEPLAY: return renderGameplay();
+        case PossibleGameStates.GAMEOVER: return renderGameOver();
+    }
 
+    // for now, any other result should just return an empty component i guess... shouldn't happen if the 
+    // game is working properly
     return (
         <div className="Game">
-
-
+            <p>Error!</p>
         </div>
     );
 }
