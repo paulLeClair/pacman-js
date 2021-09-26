07dd4669abca688f3bdca763482979913fe2b215
paul leclair <paulleclair96@gmail.com>
2021-09-26 15:24:12 -0600

    lets see how this type of setup works
    i'm not sure all this extra logic is worth it, or if i should just hardcode the orientations too in a big map, but specifying the map alone is tedious enough
    
    another alternative is to use multiple images for each tile that are pre-rotated and choose the correct one for each tile. but i feel like its better to store fewer images in the end

--- src/components/Board/BoardUtils.js
+++ src/components/Board/BoardUtils.js
@@ -1,10 +1,39 @@
 import { Orientation } from '../Game/Game'
-import { TileType } from '../Tile/Tile'
+import Tile, { TileType } from '../Tile/Tile'
+
+// now we handle the wall cases (where the orientation matters most)
+const isCorner = (tileType) => {
+    return (tileType === TileType.INNER_CORNER
+            || tileType === TileType.BOUNDARY_INCORNER
+            || tileType === TileType.BOUNDARY_OUTCORNER
+            || tileType === TileType.GB_CORNER);
+}
+
+const isEdge = (tileType) => {
+    return (tileType === TileType.INNER_EDGE
+            || tileType === TileType.BOUNDARY_EDGE);
+}
+
+const isUpperLeftCorner = (tileType) => {
+
+}
+
+const isUpperRightCorner = (tileType) => {
+
+}
+
+const isLowerLeftCorner = (tileType) => {
+
+}
+
+const isLowerRightCorner = (tileType) => {
+
+}
 
 export const computeTileImageOrientations = (mapSpecification) => {
-    return mapSpecification.map((row, x) => {
+    return mapSpecification.map((row, x) => 
         // here we look at each tile and determine the orientation it should have based on its neighbors in the grid
-        return row.map((tileType, y) => {
+        row.map((tileType, y) => {
             // i need to decide on an arbitrary "basis" type thing for rotation; i think the images will 
             // each be oriented to the user's RIGHT by default, so we just determine which rotation to use;
 
@@ -15,9 +44,62 @@ export const computeTileImageOrientations = (mapSpecification) => {
                 return Orientation.UP; // these should always be upward i think
             }
 
-            // now we handle the wall cases (where the orientation matters most)
-            
+            if (isCorner(tileType)) {
+                // in this case, we have to determine the orientation based on the type of corner
+                if (isUpperLeftCorner(tileType)) {
+                    return Orientation.RIGHT;
+                }
+                if (isUpperRightCorner(tileType)) {
+                    return Orientation.DOWN;
+                }
+                if (isLowerRightCorner(tileType)) {
+                    return Orientation.LEFT;
+                }
+                if (isLowerLeftCorner(tileType)) {
+                    return Orientation.UP;
+                }
+            }
 
+            if (isEdge(tileType)) {
+                // in this case, we determine the orientation based on where the adjacent open space is
+
+                // check "left" and "right" neighbors
+                if (x - 1 > 0) {
+                    let leftNeighbor = mapSpecification[x - 1][y];
+                    
+                    if (leftNeighbor === TileType.OPEN) {
+                        return Orientation.LEFT;
+                    }
+                }
+
+                if (x + 1 < row.length) {
+                    let rightNeighbor = mapSpecification[x + 1][y];
+
+                    if (rightNeighbor === TileType.OPEN) {
+                        return Orientation.RIGHT;
+                    }
+                }
+
+                // check "above" and "below" neighbors
+                if (y - 1 > 0) {
+                    let belowNeighbor = mapSpecification[x][y - 1];
+
+                    if (belowNeighbor === TileType.OPEN) {
+                        return Orientation.DOWN;
+                    }
+                }
+
+                if (y + 1 < mapSpecification.length) {
+                    let aboveNeighbor = mapSpecification[x][y + 1];
+
+                    if (aboveNeighbor === TileType.OPEN) {
+                        return Orientation.UP;
+                    }
+                }
+            }
+
+            // default: return right
+            return Orientation.RIGHT;
         })
-    })
+    );
 }
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Tile.scss'
+import { Orientation } from '../Game/Game'
 
 export const TileType = {
     OFFMAP: 0,
@@ -10,7 +11,6 @@ export const TileType = {
     BOUNDARY_BUMP: 5,
     INNER_EDGE: 6,
     INNER_CORNER: 7,
-    GB_EDGE: 8,
     GB_CORNER: 9,
     GB_DOOR: 10
 };
@@ -20,11 +20,79 @@ export const TileSize = 12; // tile size in pixels, will probably need to be twe
 // to save space, we'll have to carefully manage orientations of tiles to rotate them according to their surroundings 
 
 const Tile = ({ orientation, xPos, yPos, type }) => {
-    // this will encode a single 
+    // to orient each image, maybe i'll make 4 different possible CSS classes so that 
+    // we just select the one based on the orientation?
+
+    let orientationClass = "orientation-right"
+    switch (orientation) {
+        case Orientation.RIGHT: orientationClass = "orientation-right"; break; 
+        case Orientation.DOWN: orientationClass = "orientation-down"; break;
+        case Orientation.LEFT: orientationClass = "orientation-left"; break;
+        case Orientation.UP: orientationClass = "orientation-up"; break;
+    }
+
+    let tileImagePath = "../../assets/";
+    switch (type) {
+        case TileType.OFFMAP: {
+            tileImagePath += "offmap.jpg"
+            break;
+        }
+
+        case TileType.OPEN: {
+            tileImagePath += "open.jpg"
+            break;
+        }
+
+        case TileType.BOUNDARY_EDGE: {
+            tileImagePath += "boundary-edge.jpg"
+            break;
+        }
+
+        case TileType.BOUNDARY_INCORNER : {
+            tileImagePath += "boundary-incorner.jpg"
+            break;
+        }
+
+        case TileType.BOUNDARY_OUTCORNER: {
+            tileImagePath += "boundary-outcorner.jpg"
+            break;
+        }
+
+        case TileType.BOUNDARY_BUMP: {
+            tileImagePath += "boundary-bump.jpg"
+            break;
+        }
+
+        case TileType.INNER_CORNER: {
+            tileImagePath += "inner-corner.jpg"
+            break;
+        }
+
+        case TileType.INNER_EDGE: {
+            tileImagePath += "inner-edge.jpg"
+            break;
+        }
+
+        case TileType.GB_CORNER : {
+            tileImagePath += "gb-corner.jpg"
+            break;
+        }
+
+        case TileType.GB_DOOR : {
+            tileImagePath += "gb-door.jpg"
+            break;
+        }
+    }
+
+    // i'll set the position using an inline style variable
+    const posStyles = {
+        left: toString(xPos) + "px",
+        top: toString(yPos) + "px",
+    };
 
     return (
         <div className="tile">
-
+            <img className={orientationClass} style={posStyles} src={tileImagePath} />
         </div>
     );
 }
